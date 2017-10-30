637265c1dcb24f190bdc9260a50435f32c746183
jzsstrj@gmail.com <tangrj@ricelink.cn>
2017-10-30 23:03:43 +0800

    创建出循环显示的效果

--- src/main/java/demo/plan/controller/PlanController.java
+++ src/main/java/demo/plan/controller/PlanController.java
@@ -1,14 +1,15 @@
 package demo.plan.controller;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -17,9 +18,10 @@ import com.alibaba.fastjson.JSONObject;
 @Controller
 public class PlanController {
 
-	@RequestMapping("/helloHtml")
+	@RequestMapping("/hello")
     public String helloHtml(Map<String,Object> map){
        map.put("hello","from TemplateController.helloHtml");
+       map.put("myself","yes1");
        return"/hello";
     }
 	
@@ -31,15 +33,17 @@ public class PlanController {
 	
 	@RequestMapping("/index2")
     public String index(Model map){
-		JSONObject obj = new JSONObject();
 		JSONObject plan = new JSONObject();
+		List<JSONObject> plans = new ArrayList<JSONObject>();
 		plan.put("name", "trj's plan");
 		plan.put("date", new SimpleDateFormat("yyyy-MM-dd hh-mm-ss").format(new Date()));
 		plan.put("point", "50");
 		plan.put("state", "1");
-		obj.put("plan", plan);
+//		plans.add(plan);
+//		plans.add(plan);
 		map.addAttribute("myword","THE WORLD TOKIOTOMALEI!");
 		map.addAttribute("plan", plan);
+		map.addAttribute("plans", plans);
        return"/index";
     }
 }
---
+++ src/main/java/demo/plan/core/test.java
@@ -0,0 +1,16 @@
+package demo.plan.core;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {49,105,53,74,51,116,100,106};
+		String b = "";
+		for (int i : a) {
+			char c = (char)i;
+			b += c;
+		}
+		System.out.println(b);
+	}
+
+}
